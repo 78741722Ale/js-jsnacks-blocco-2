f1e907247788fca067a77ac420f0b485f30d164c
78741722Ale <pecorillaalessandro@gmail.com>
2022-03-31 18:14:48 +0200

    Snack 4 - Aggiunto nuove modifiche per verifica

--- snack-4/js/main.js
+++ snack-4/js/main.js
@@ -3,22 +3,47 @@ Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in 
 */
 
 // Array di numeri interi
-const arrayNumbers = [11, 10, 17, 18, 23, 25, 28, 30];
+const arrayNumbers = [7, 6, 17, 17, 23, 25, 27, 31];
 console.log(arrayNumbers);
 
 // sommma già a 0
 let somma = 0;
 console.log(typeof somma);
 
+// Array vuoto per aggiungere numero
+const arrayNull = [];
+
 // Variabile I
 let i = 0;
 console.log(typeof i);
 
-while (i <= arrayNumbers.length) {
+// Valore booleano, settato su false
+let control = false;
+
+while (i < arrayNumbers.length) {
     // Risoluzione del problema
-    if (i % 2 !== 0) {
-        somma = somma + arrayNumbers[i];
-    } 
+    if (i % 10 !== 0) {
+        // Variabile Boolean settata su True
+        control = true;
+        // Variabile somma dichiarata
+        somma += arrayNumbers[i];
+    } else if (i % 5 === 0) {
+        control = true;
+        // Variabile somma dichiarata
+        somma += arrayNumbers[i];
+    } else if (i % 3 === 0) {
+        control = true;
+        // Variabile somma dichiarata
+        somma += arrayNumbers[i];
+    } else if (i % 2 !== 1) {
+        // Variabile Boolean settata su True
+        control = true;
+        // Variabile somma dichiarata
+        somma += arrayNumbers[i];
+    }
+
+    arrayNull.push(somma);
+    console.log(somma);
     // Numero di Volte da ripetere
     i++
 }
