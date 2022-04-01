decfd7feb17a2bff8a3b61db728e3be4839bf455
78741722Ale <pecorillaalessandro@gmail.com>
2022-04-01 14:10:31 +0200

    Aggiunta correzione in classe tramite live coding

--- snack-2/js/main.js
+++ snack-2/js/main.js
@@ -17,18 +17,36 @@ const userNumber = parseInt(document.getElementById("userNumber").value);
 console.log(typeof userNumber);
 
 // Risoluzione con Ciclo While
-console.log("Ciclo While");
-let i = 0;
-while (i <= 2) {
-    if (userNumber % 2 === 0) {
-        // Stampo il numero in console log
-        console.log(`Il numero è ${userNumber}`);
-    } else {
-        let somma = userNumber + variabile;
-        // Stampo il numero in console log
-        console.log(`Il numero è ${somma}`);
-    }
-    i++
+// console.log("Ciclo While");
+// let i = 0;
+// while (i <= 2) {
+//     if (userNumber % 2 === 0) {
+//         // Stampo il numero in console log
+//         console.log(`Il numero è ${userNumber}`);
+//     } else {
+//         let somma = userNumber + variabile;
+//         // Stampo il numero in console log
+//         console.log(`Il numero è ${somma}`);
+//     }
+//     i++
+// }
+
+
+// Senza Ciclo While
+
+// Numero dichiarato nell'input
+let userNumber_2 = 16;
+console.log(typeof userNumber_2);
+
+if (userNumber_2 % 2 === 0) {
+    // Stampo il numero in console log
+    console.log(`Il numero è ${userNumber_2}`);
+} else {
+    // Stampo il numero in console log
+    console.log(`Il numero è ${++userNumber_2}`);
+
+    
+
 }
 
 
